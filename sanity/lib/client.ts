bd910bf0c2b15122c148f8b1b99a31e4894b4478
Amr <msam.team20@gmail.com>
2025-01-06 10:34:18 +0200

    Sanity production env handle

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -8,6 +8,6 @@ export const client = createClient({
   apiVersion,
   useCdn: true,
   stega: {
-    studioUrl: process.env.NODE_ENV === 'production' ? `https://${process.env.VERCEL_URL}/studio` : 'localhost:3000/studio',
+    studioUrl: process.env.NODE_ENV === 'production' ? `https://estoreapp-sage.vercel.app/studio` : 'localhost:3000/studio',
   }, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
