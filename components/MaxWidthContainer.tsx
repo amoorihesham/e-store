8a56ccdd90770a7a8a2dd8291304cc28c8f69aa8
Amr <msam.team20@gmail.com>
2025-06-16 13:21:29 +0300

    upgrade: ui component

---
+++ components/MaxWidthContainer.tsx
@@ -0,0 +1,6 @@
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+
+export default function MaxWidthContainer({ children, className }: { children: ReactNode; className?: string }) {
+  return <div className={cn(className, 'max-w-[1560px] mx-auto px-4')}>{children}</div>;
+}
