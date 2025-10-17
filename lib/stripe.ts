4c0644aefb3572963c699c8e5c7633038b9e103b
amoorihesham <amrheshammacc@gmail.com>
2025-10-17 16:19:12 +0300

    solve stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,11 @@
-import { Stripe } from 'stripe';
+import { Stripe } from "stripe";
 
 if (!process.env.STRIPE_SECRET!) {
-  throw new Error('No Stripe Secret');
+  throw new Error("No Stripe Secret");
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET!, {
-  apiVersion: '2025-08-27.basil',
+  apiVersion: "2025-09-30.clover",
 });
 
 export default stripe;
